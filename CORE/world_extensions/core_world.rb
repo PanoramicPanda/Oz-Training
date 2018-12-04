c15b34d5fb4e65b5e2df57637275998cd5fc93c7
Uriah Brown <uriah.h.brown@gmail.com>
2018-12-04 08:45:42 -0500

    Adding Gemfile to lock Watir, just to not deal with .present logic changes

--- CORE/world_extensions/core_world.rb
+++ CORE/world_extensions/core_world.rb
@@ -2,7 +2,7 @@ require 'watir'
 
 module CoreWorld
 
-    attr_accessor :browser, :configuration, :ledger, :router, :data_engine, :browser_engine, :validation_engine
+    attr_accessor :browser, :configuration, :ledger, :router, :data_engine, :browser_engine, :validation_engine, :watir_version
     attr_reader :root_page, :data_target, :logger
 
     def create_world
@@ -13,6 +13,7 @@ module CoreWorld
         @router = Router.new(self)
         @browser_engine = BrowserEngine.new(self)
         @validation_engine = ValidationEngine.new(self)
+        @watir_version = Gem.loaded_specs['watir'].version >= Gem::Version.create('6.12')
         log_header
         set_data_target
         set_default_watir_timeout
---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem 'cucumber'
+gem 'watir', '6.8.4'
