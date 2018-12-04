a5a525514dd43dc4fed1f4c99ffbe28898ef7c95
PanoramicPanda <uriah.h.brown@gmail.com>
2018-12-04 16:46:31 -0500

    Adding overrides to setup

--- SELENIUM-EASY/setup.rb
+++ SELENIUM-EASY/setup.rb
@@ -7,6 +7,9 @@ require_relative '../CORE/setup.rb'
 
 require_all('../SELENIUM-EASY/overrides/elements')
 
+require_all('../SELENIUM-EASY/overrides/elements')
+require_all('../SELENIUM-EASY/overrides/world_extensions')
+
 require_relative '../SELENIUM-EASY/pages/selenium_easy_root_page.rb'
 recursively_require_all_base_pages('../SELENIUM-EASY/pages')
 recursively_require_all_edge_pages('../SELENIUM-EASY/pages')
