cfe14a9cc973543cbd8c6fc1575e108641ef1b27
PanoramicPanda <uriah.h.brown@gmail.com>
2018-12-04 19:55:36 -0500

    Adding various element validation

---
+++ SELENIUM-EASY/overrides/elements/checkbox.rb
@@ -0,0 +1,15 @@
+class CheckboxElement < CoreElement
+
+  def validate(data)
+    super(data)
+    if active
+      validation_point = @world.validation_engine.add_validation_point("Checking that [#{@name}] is [#{data}]")
+      if value == data
+        validation_point.pass
+      else
+        validation_point.fail("ERROR! [#{@name}] has incorrect value!\n\tFOUND   : #{value.inspect}\n\tEXPECTED: #{data.inspect}")
+      end
+    end
+  end
+
+end
