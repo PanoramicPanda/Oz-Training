c7d07c8afc3c4963432fa50d24f365e773b0df01
PanoramicPanda <uriah.h.brown@gmail.com>
2018-12-04 20:38:26 -0500

    Correcting route and default

--- SELENIUM-EASY/pages/demo_home_page.rb
+++ SELENIUM-EASY/pages/demo_home_page.rb
@@ -3,7 +3,7 @@ class DemoHomePage < SeleniumEasyRootPage
   add_id_element(:div, /WELCOME TO SELENIUM EASY DEMO/i, id: 'home')
 
   add_route(:SimpleFormDemoPage, :to_simple_form_page)
-  add_route(:SimpleFormDemoPage, :to_checkbox_demo_page)
+  add_route(:CheckboxDemoPage, :to_checkbox_demo_page)
   add_route(:RadioButtonsDemoPage, :to_radio_buttons_demo_page)
   add_route(:SelectDropdownListPage, :to_select_dropdown_list_page)
 
