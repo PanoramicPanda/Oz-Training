5595558f3bd5c07dffa7ef6b71b92ae0f6da53ce
PanoramicPanda <uriah.h.brown@gmail.com>
2018-12-12 21:16:26 -0500

    Code

--- SELENIUM-EASY/pages/input_forms/radio_buttons_demo_page.rb
+++ SELENIUM-EASY/pages/input_forms/radio_buttons_demo_page.rb
@@ -30,6 +30,50 @@ class RadioButtonsDemoPage < SeleniumEasyRootPage
       @world.ledger.record_fill(rb_demo_display.name, message)
     end
 
+    create_group_radio_buttons_demo
+
+  end
+
+  def create_group_radio_buttons_demo
+    group_age_0_5 = add_radio_button(:group_age_0_5, xpath: '//*[@id="easycont"]/div/div[2]/div[2]/div[2]/div[2]/label[1]/input')
+    group_age_5_15 = add_radio_button(:group_age_5_15, xpath: '//*[@id="easycont"]/div/div[2]/div[2]/div[2]/div[2]/label[2]/input')
+    group_age_15_50 = add_radio_button(:group_age_15_50, xpath: '//*[@id="easycont"]/div/div[2]/div[2]/div[2]/div[2]/label[3]/input')
+
+    get_group_values = add_button(:get_group_values, text: 'Get values')
+
+    group_text = add_static_text(:group_text, element_type: :p, id: 'groupradiobutton')
+    group_text.deactivate
+
+    group_age_0_5.on_fill do
+      @world.ledger.record_fill(group_age_5_15.name, 'unselected')
+      @world.ledger.record_fill(group_age_15_50.name, 'unselected')
+    end
+
+    group_age_5_15.on_fill do
+      @world.ledger.record_fill(group_age_0_5.name, 'unselected')
+      @world.ledger.record_fill(group_age_15_50.name, 'unselected')
+    end
+
+    group_age_15_50.on_fill do
+      @world.ledger.record_fill(group_age_0_5.name, 'unselected')
+      @world.ledger.record_fill(group_age_5_15.name, 'unselected')
+    end
+
+    get_group_values.on_click do
+      group_text.activate
+
+      if @world.ledger.get_value(self.class, group_age_0_5.name) == 'selected'
+        age = '0 - 5'
+      elsif @world.ledger.get_value(self.class, group_age_0_5.name) == 'selected'
+        age = '5 - 15'
+      elsif  @world.ledger.get_value(self.class, group_age_0_5.name) == 'selected'
+        age = '15 - 50'
+      else
+        age = ''
+      end
+
+      @world.ledger.record_fill(group_text.name, "Sex : \n Age group: #{age}")
+    end
   end
 
 end
